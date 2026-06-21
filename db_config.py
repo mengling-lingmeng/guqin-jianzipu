4c3002dafa54281bbbab9de879c907d784a1e356
menling-lingmeng <mengling-lingmeng@example.com>
2026-06-21 11:31:36 +0800

    初始提交：古琴减字谱系统

--- db_config.py
+++ db_config.py
@@ -1,7 +1,10 @@
+import os
+
 DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '040508',
-    'database': 'guqin_jianzipu',
-    'charset': 'utf8mb4'
+    'host': os.environ.get('MYSQL_HOST', 'localhost'),
+    'user': os.environ.get('MYSQL_USER', 'root'),
+    'password': os.environ.get('MYSQL_PASSWORD', ''),
+    'database': os.environ.get('MYSQL_DATABASE', 'guqin_jianzipu'),
+    'charset': 'utf8mb4',
+    'ssl': None
 }
