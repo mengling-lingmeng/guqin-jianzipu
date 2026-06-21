f503999574499c7af38cfcdcfc385eeeac6329a1
menling-lingmeng <mengling-lingmeng@example.com>
2026-06-21 15:01:44 +0800

    更新说明：修复了功能

--- db_config.py
+++ db_config.py
@@ -3,7 +3,7 @@ import os
 DB_CONFIG = {
     'host': os.environ.get('MYSQL_HOST', 'localhost'),
     'user': os.environ.get('MYSQL_USER', 'root'),
-    'password': os.environ.get('MYSQL_PASSWORD', ''),
+    'password': os.environ.get('MYSQL_PASSWORD', '040508'),
     'database': os.environ.get('MYSQL_DATABASE', 'guqin_jianzipu'),
     'charset': 'utf8mb4',
     'ssl': None
